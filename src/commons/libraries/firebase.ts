addf970c037ac3d9e52687bb6c05c397ed731b35
SoonHyunAn <155404398+SoonHyunAn@users.noreply.github.com>
2024-04-14 19:54:19 +0900

    Update firebase.ts

--- src/commons/libraries/firebase.ts
+++ src/commons/libraries/firebase.ts
@@ -7,13 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDgkFaGyM5kFIm5Vnv9DZRapLfsPMAQr4A",
-  authDomain: "fir-test-49ac8.firebaseapp.com",
-  projectId: "fir-test-49ac8",
-  storageBucket: "fir-test-49ac8.appspot.com",
-  messagingSenderId: "1054947533888",
-  appId: "1:1054947533888:web:868d7c21ec62eef8e82ba5",
-  measurementId: "G-99LEBSFK03"
+
 };
 
 // Initialize Firebase
