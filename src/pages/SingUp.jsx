95ecc543bc3af409e3363a73ce77891daf484c7e
SoonHyunAn <ash7106@gmail.com>
2024-04-15 10:14:34 +0900

    firebase

--- src/pages/SingUp.jsx
+++ src/pages/SingUp.jsx
@@ -9,7 +9,9 @@ export default function SignUp() {
   return (
     <div style={{ margin: '20px' }}>
       <form onSubmit={handleSubmit}>
-
+      <input type="email" name="email" value={userInfo.email} placeholder="이메일" onChange={handleChange}/><br />
+      <input type="password" name="password" value={userInfo.password} placeholder="패스워드" onChange={handleChange}/><br />
+      <button onClick={handleSubmit}>사용자 등록</button>
       </form>
     </div>
   );
