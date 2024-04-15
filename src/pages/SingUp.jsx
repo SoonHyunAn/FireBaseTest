f8a72148ed69e070a169b35f7d2087355efe5de3
SoonHyunAn <ash7106@gmail.com>
2024-04-15 10:11:27 +0900

    firebase

--- src/pages/SingUp.jsx
+++ src/pages/SingUp.jsx
@@ -3,8 +3,14 @@ import { register } from "../api/firebase.js";
 
 export default function SignUp() {
   const [userInfo, setUserInfo] = useState({ email: '', password: '' });
+  const [user, setUser] = useState(null);
+
 
   return (
+    <div style={{ margin: '20px' }}>
+      <form onSubmit={handleSubmit}>
 
+      </form>
+    </div>
   );
 }
